c8df552a70c37cb0c504e026630afa216053774c
Daniel Lamb <daniellamb113@gmail.com>
2019-09-16 19:09:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,40 +1,40 @@
-def create_an_empty_array
-  []
-end
+# def create_an_empty_array
+#   []
+# end
 
-def create_an_array
-   array = ["wow", "I", "am", "really", "learning"]
+# def create_an_array
+#   array = ["wow", "I", "am", "really", "learning"]
  
-end
+# end
 
-def add_element_to_end_of_array(array, element)
-   array = ["wow", "I", "am", "really", "learning"]
-    element = "arrays!"
-end
+# def add_element_to_end_of_array(array, element)
+#   array = ["wow", "I", "am", "really", "learning"]
+#     element = "arrays!"
+# end
 
-def add_element_to_start_of_array(array, element)
-   array = ["wow", "I", "am", "really", "learning"]
-    element = "arrays!"
-end
+# def add_element_to_start_of_array(array, element)
+#   array = ["wow", "I", "am", "really", "learning"]
+#     element = "arrays!"
+# end
 
-def remove_element_from_end_of_array(array)
-   array = ["wow", "I", "am", "really", "learning"]
+# def remove_element_from_end_of_array(array)
+#   array = ["wow", "I", "am", "really", "learning"]
    
-end
+# end
 
-def remove_element_from_start_of_array(array)
-   array = ["wow", "I", "am", "really", "learning"]
+# def remove_element_from_start_of_array(array)
+#   array = ["wow", "I", "am", "really", "learning"]
  
-end
+# end
 
-def retrieve_element_from_index(array, index_number)
-   array = ["1, "2, "3, "4", "0"]
-    element = "arrays!"
-end
+# def retrieve_element_from_index(array, index_number)
+#   array = ["1, "2, "3, "4", "0"]
+#     element = "arrays!"
+# end
 
-def retrieve_first_element_from_array(array)
+# def retrieve_first_element_from_array(array)
   
-end
+# end
 
 def retrieve_last_element_from_array(array)
   
